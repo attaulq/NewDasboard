7f66a6f293cc8d08f4c3663922001a5e16c7415f
attaulq <79440334+attaulq@users.noreply.github.com>
2026-05-03 23:33:31 +0700

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -8,7 +8,7 @@ import plotly.express as px
 # =========================
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 #from utils.auto_rotate import run_auto_rotate
-run_auto_rotate()
+#run_auto_rotate()
 # =========================
 # PATH ROOT
 # =========================
